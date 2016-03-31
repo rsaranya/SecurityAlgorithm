12a4172ed3715226d70bd10c1a6c02ef350a28b1
Saranya1904 <Saranya.Xx1@marist.edu>
2016-03-31 07:36:43 -0400

    Added Lab03 java files

---
+++ Lab03/Driver.java
@@ -0,0 +1,48 @@
+package driver;
+
+
+/**
+ * @author Saranya
+ * @CWID 20062589
+ * @Program Utilizes the AES Key generation class to generate round keys from an
+ */
+
+import java.util.*;
+
+public class Driver {
+
+  /**
+   * Is the Entry point to the program Fetches the key from the user and calls
+   * the Round key generator.
+   *
+   * @param args the command line argument
+   */
+  public static void main(String[] args) {
+    try {
+      //Takes input from user and stores in inputkey
+      Scanner in1 = new Scanner(System.in);
+      String inputKey = in1.nextLine();
+      String inputPlainText = "";
+      
+      if(in1.hasNext())
+        inputPlainText = in1.nextLine();
+      //Takes 2nd input from user and stores in inputPlainText
+      
+      //If condition makes sure that user has entered exactly 32 hexadecimal digits
+      if ((inputKey.matches("[0-9A-F]{32}")) && (inputPlainText.matches("[0-9A-F]{32}"))) {
+        //Call the aesKeyGen class and pass the plain text and input key as string to the constructor
+        AesCipher aesKeyGen = new AesCipher();
+        //Call the aes function to process the input key
+        //and generate 10 more round keys
+        aesKeyGen.aes(inputPlainText, inputKey);
+
+      } else {
+        //If user input is incorrect, terminate the program
+        System.out.println("Invalid input key or plaintext, exiting.....");
+        System.exit(0);
+      }
+    } catch (Exception ex) {
+      System.out.println("Exception in main is : " + ex);
+    }
+  }
+}
