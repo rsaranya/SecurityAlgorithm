9891051a4f0f120750a0c055fa0ddc4047d501f6
suryavalluri1 <surya.valluri1@marist.edu>
2016-04-28 19:32:40 -0400

    Added aproject which connects to DB using Hibernate Framework

---
+++ HibernateProject/SysAnatomy/src/main/java/WindowsService/persistence/HibernateUtil.java
@@ -0,0 +1,31 @@
+package WindowsService.persistence;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+  private static final SessionFactory sessionFactory = buildSessionFactory();
+
+  private static SessionFactory buildSessionFactory() {
+      try {
+          // Create the SessionFactory from hibernate.cfg.xml
+          Configuration conf = new Configuration();
+          conf = conf.configure();
+          return conf.buildSessionFactory();
+      }
+      catch (Throwable ex) {
+          // Make sure you log the exception, as it might be swallowed
+          System.err.println("Initial SessionFactory creation failed." + ex);
+          throw new ExceptionInInitializerError(ex);
+      }
+  }
+
+  public static SessionFactory getSessionFactory() {
+      return sessionFactory;
+  }
+  
+  public static void shutdown() {
+    // Close caches and connection pools
+    getSessionFactory().close();
+  }
+}
